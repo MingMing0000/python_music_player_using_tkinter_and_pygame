ec9cd52a188e5fd83aa85a236ded33643bf03d16
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-06 19:56:28 +0800

    define the title and geometry, initiate pygame and pygame mixer, add track and status variable

--- music_player.py
+++ music_player.py
@@ -5,4 +5,18 @@ import os
 
 # Create MusicPlayer Class
 class MusicPlayer():
-    pass
+    
+    def __init__(self, root):
+        self.root = root
+        # Title of the window
+        self.root.title("MusicPlayer")
+        # Window Geometry
+        self.root.geometry("1000x200+200+200")
+        # Initiating Pygame
+        pygame.init()
+        # Initiating Pygame Mixer
+        pygame.mixer.init()
+        # Declaring track Variable
+        self.track = StringVar()
+        # Declaring Status Variable
+        self.status = StringVar()
