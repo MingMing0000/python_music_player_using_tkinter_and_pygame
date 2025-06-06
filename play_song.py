611f7283d9eff6bb99586a2ac2e6b7388683cd5d
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-06 21:35:01 +0800

    create PlaySong class

--- play_song.py
+++ play_song.py
@@ -2,3 +2,7 @@
 import pygame
 from tkinter import *
 from music_player import MusicPlayer
+
+# create a class PlaySong that inherits from the class MusicPlayer
+class PlaySong(MusicPlayer):
+    pass
