0964dd73d5925a11f7f0973c6eb5efc39a0ec7d4
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-06 21:39:30 +0800

    add method named play_song

--- play_song.py
+++ play_song.py
@@ -9,3 +9,11 @@ class PlaySong(MusicPlayer):
     def set_status(self):
         # Displaying Status
         self.status.set("-Playing")
+
+    def play_song(self):
+        # Displaying Selected Song title
+        self.track.set(self.playlist.get(ACTIVE))
+        # Loading Selected Song
+        pygame.mixer.music.load(self.playlist.get(ACTIVE))
+        # Playing Selected Song
+        pygame.mixer.music.play()
