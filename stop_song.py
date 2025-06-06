8432732064aa5c3a509a77b9f50e08b361e36e32
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-06 21:48:36 +0800

    create class named StopSong

--- stop_song.py
+++ stop_song.py
@@ -2,3 +2,7 @@
 import pygame
 from music_controls import MusicControls
 from tkinter import *
+
+# create a class StopSong that inherits from the class MusicControls
+class StopSong(MusicControls):
+    pass
