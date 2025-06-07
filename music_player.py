d5a98be7cf76eed18afb0f26e4a653a77c775da2
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-07 13:53:17 +0800

    fix the namings

--- music_player.py
+++ music_player.py
@@ -26,42 +26,42 @@ class MusicPlayer():
         self.status = StringVar()
 
         # Creating the Track Frames for Song label & status label
-        trackframe = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="Navyblue",fg="white",bd=5,relief=GROOVE)
-        trackframe.place(x=0,y=0,width=600,height=100)
+        track_frame = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="Navyblue",fg="white",bd=5,relief=GROOVE)
+        track_frame.place(x=0,y=0,width=600,height=100)
         # Inserting Song Track Label
-        songtrack = Label(trackframe,textvariable=self.track,width=20,font=("times new roman",24,"bold"),bg="Orange",fg="gold").grid(row=0,column=0,padx=10,pady=5)
+        song_track = Label(track_frame,textvariable=self.track,width=20,font=("times new roman",24,"bold"),bg="Orange",fg="gold").grid(row=0,column=0,padx=10,pady=5)
         # Inserting Status Label
-        trackstatus = Label(trackframe,textvariable=self.status,font=("times new roman",24,"bold"),bg="orange",fg="gold").grid(row=0,column=1,padx=10,pady=5)
+        track_status = Label(track_frame,textvariable=self.status,font=("times new roman",24,"bold"),bg="orange",fg="gold").grid(row=0,column=1,padx=10,pady=5)
 
         # Creating Button Frame
         buttonframe = LabelFrame(self.root,text="Control Panel",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
         buttonframe.place(x=0,y=100,width=600,height=100)
         # Inserting Play Button
-        playbtn = Button(buttonframe,text="PLAYSONG",command=self.playsong,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=0,padx=10,pady=5)
+        play_button = Button(buttonframe,text="PLAYSONG",command=self.play_song,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=0,padx=10,pady=5)
         # Inserting Pause Button
-        playbtn = Button(buttonframe,text="PAUSE",command=self.pausesong,width=8,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=1,padx=10,pady=5)
+        play_button = Button(buttonframe,text="PAUSE",command=self.pause_song,width=8,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=1,padx=10,pady=5)
         # Inserting Unpause Button
-        playbtn = Button(buttonframe,text="UNPAUSE",command=self.unpausesong,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=2,padx=10,pady=5)
+        play_button = Button(buttonframe,text="UNPAUSE",command=self.unpause_song,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=2,padx=10,pady=5)
         # Inserting Stop Button
-        playbtn = Button(buttonframe,text="STOPSONG",command=self.stopsong,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=3,padx=10,pady=5)
+        play_button = Button(buttonframe,text="STOPSONG",command=self.stop_song,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="pink").grid(row=0,column=3,padx=10,pady=5)
 
         # Creating Playlist Frame
-        songsframe = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-        songsframe.place(x=600,y=0,width=400,height=200)
+        songs_frame = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
+        songs_frame.place(x=600,y=0,width=400,height=200)
         # Inserting scrollbar
-        scrol_y = Scrollbar(songsframe,orient=VERTICAL)
+        scroll_y = Scrollbar(songs_frame,orient=VERTICAL)
         # Inserting Playlist listbox
-        self.playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
+        self.playlist = Listbox(songs_frame,yscrollcommand=scroll_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
         # Applying Scrollbar to listbox
-        scrol_y.pack(side=RIGHT,fill=Y)
-        scrol_y.config(command=self.playlist.yview)
+        scroll_y.pack(side=RIGHT,fill=Y)
+        scroll_y.config(command=self.playlist.yview)
         self.playlist.pack(fill=BOTH)
         # Changing Directory for fetching Songs
         os.chdir("PATH/OF/DIRECTORY")
         # Fetching Songs
-        songtracks = os.listdir()
+        song_tracks = os.listdir()
         # Inserting Songs into Playlist
-        for track in songtracks:
+        for track in song_tracks:
             self.playlist.insert(END,track)
 
     def play_song(self):
