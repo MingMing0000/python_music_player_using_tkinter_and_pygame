db72d5698bc2d8051baed9cdac364c749734b51e
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-07 16:36:28 +0800

    execute the program

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # import the needed files
 from music_player import MusicPlayer
 from tkinter import *
+
+if __name__ == "__main__":
+    root = Tk() # In order to create an empty window
+# Passing Root to MusicPlayer Class
+MusicPlayer(root)
+
+root.mainloop()
