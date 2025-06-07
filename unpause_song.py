af87699f781c0c827289875c995c245fc182381f
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-07 11:44:43 +0800

    create UnpauseSong class

--- unpause_song.py
+++ unpause_song.py
@@ -2,3 +2,7 @@
 import pygame
 from music_controls import MusicControls
 from tkinter import *
+
+# create a class UnpauseSong that inherits from the class MusicControls
+class UnpauseSong(MusicControls):
+    pass
