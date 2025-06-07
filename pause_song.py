e1fb4ca1c2563da46910d7fbbf500248b77ff859
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-06-07 11:29:47 +0800

    create PauseSong class

--- pause_song.py
+++ pause_song.py
@@ -2,3 +2,7 @@
 import pygame
 from music_controls import MusicControls
 from tkinter import *
+
+# create a class PauseSong that inherits from the class MusicControls
+class PauseSong(MusicControls):    
+    pass
